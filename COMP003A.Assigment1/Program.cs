22956eea7724e4e1637b08410c92c43d7add9cf9
leonelpaz <leonelpaz@LEM231-29>
2023-01-18 19:27:14 -0800

    Add project files.

---
+++ COMP003A.Assigment1/Program.cs
@@ -0,0 +1,17 @@
+﻿// this is a single-line comment
+/*  this is a multi-line comment
+ *  
+ *  Author:     Leonel Paz
+ *  Course:     COMP-003A-L01
+ *  Purpose:    Code for Assignment 1
+ *  */
+namespace COMP003A.Assigment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
